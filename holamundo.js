05cafe51cf6fd21d6b51512b7b8950561b29d4e1
jhoanmceron-oss <jhoanmceron@gmail.com>
2026-02-13 03:33:59 -0400

    Add files via upload
    Agregar hola-mundo.js

---
+++ holamundo.js
@@ -0,0 +1,7 @@
+// Programa: Hola Mundo en JavaScript
+// Practica DevOps - Integracion Continua con GitHub Actions
+
+console.log("¡Hola Mundo!");
+console.log("Este programa fue ejecutado automaticamente por GitHub Actions.");
+console.log("Repositorio: practica-devops");
+console.log("Rama: main");
